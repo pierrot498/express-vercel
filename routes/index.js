2df93f26cba2e51d27cfe2966225086c8e8c133e
pierrot498 <pierre.etienne.horreau@gmail.com>
2022-10-31 22:04:52 +0100

    win

--- routes/index.js
+++ routes/index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
-const db = require("./app/models");
+const db = require("../models");
 db.sequelize.sync()
   .then(() => {
     console.log("Synced db.");
